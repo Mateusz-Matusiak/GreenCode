f2be96b4ac7842c042b1ca5d0613c4594d63cf5e
Mateusz Matusiak <mateusz.matusiak@likims.com>
2023-05-13 17:31:24 +0200

    Delete data mocking

--- src/main/java/pl/ingsolution/greencode/atm/service/AtmController.java
+++ src/main/java/pl/ingsolution/greencode/atm/service/AtmController.java
@@ -1,32 +1,17 @@
 package pl.ingsolution.greencode.atm.service;
 
-import com.github.javafaker.Faker;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import pl.ingsolution.greencode.atm.service.dtos.ATM;
-import pl.ingsolution.greencode.atm.service.dtos.RequestType;
 import pl.ingsolution.greencode.atm.service.dtos.Task;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public record AtmController(
         AtmService atmService
 ) {
-
-    @GetMapping("/atms")
-    public List<Task> generateData() {
-        Faker faker = new Faker();
-        List<Task> tasks = new ArrayList<>();
-        RequestType[] requestTypes = {RequestType.STANDARD, RequestType.PRIORITY, RequestType.FAILURE_RESTART, RequestType.SIGNAL_LOW};
-        for (int i = 0; i < 100_000; i++) {
-            tasks.add(new Task(faker.number().numberBetween(1, 1000), requestTypes[faker.number().numberBetween(0, 3)], faker.number().numberBetween(1, 1000)));
-        }
-        return tasks;
-    }
 
     @PostMapping("/atms/calculateOrder")
     public List<ATM> calculateOrder(@RequestBody final List<Task> tasks) {
--- src/main/java/pl/ingsolution/greencode/transaction/TransactionController.java
+++ src/main/java/pl/ingsolution/greencode/transaction/TransactionController.java
@@ -1,30 +1,17 @@
 package pl.ingsolution.greencode.transaction;
 
-import com.github.javafaker.Faker;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import pl.ingsolution.greencode.transaction.dtos.Account;
 import pl.ingsolution.greencode.transaction.dtos.Transaction;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public record TransactionController(
         TransactionService transactionService
 ) {
-
-    @GetMapping("/transactions")
-    public List<Transaction> generateTransactions() {
-        Faker faker = new Faker();
-        ArrayList<Transaction> result = new ArrayList<>();
-        for (int i = 0; i < 100_000; i++) {
-            result.add(new Transaction(faker.pokemon().name(), faker.pokemon().name(), faker.number().randomDouble(2, 10, 2000)));
-        }
-        return result;
-    }
 
     @PostMapping("/transactions/report")
     public List<Account> processAllTransactions(
